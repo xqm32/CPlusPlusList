f219dc150279e5738a5c2848311d99d81782b92c
xqm32 <458173774@qq.com>
2022-03-26 22:22:51 +0800

    add `srand(seed)`

--- src/main.cpp
+++ src/main.cpp
@@ -5,29 +5,36 @@
 #include "Sort.h"
 #include "TestList.h"
 
-#define TEST_SIZE 1024
-
 template <template <typename> typename T, typename F>
-double test(F func) {
+double test(F func, size_t n) {
     auto listA = T<int>();
     auto listB = T<int>();
-    generateList(listA, TEST_SIZE);
-    generateList(listB, TEST_SIZE);
+    generateList(listA, n);
+    generateList(listB, n);
     auto timeA = clock();
     func(listA, listB);
     auto timeB = clock();
     return 1000.0 * (timeB - timeA) / CLOCKS_PER_SEC;
 }
 
-void finishMyHomework() {
-    printf("对含有 %d 个整数的两个链表合并并排序花费 %lf ms\n", TEST_SIZE,
-           test<LinkedList>(mergeSort<int>));
-    printf("对含有 %d 个整数的两个链表排序并合并花费 %lf ms\n", TEST_SIZE,
-           test<LinkedList>(mergeSort<int>));
-    printf("对含有 %d 个整数的两个顺序表合并并排序花费 %lf ms\n", TEST_SIZE,
-           test<SequenceList>(mergeSort<int>));
-    printf("对含有 %d 个整数的两个顺序表合并并排序花费 %lf ms\n", TEST_SIZE,
-           test<SequenceList>(sortMerge<int>));
+void finishMyHomework(size_t n) {
+    auto seed = time(NULL);
+    srand(seed);
+    printf("对含有 %ld 个整数的两个链表合并后排序花费 %lf ms\n", n * 2,
+           test<LinkedList>(mergeSort<int>, n));
+    srand(seed);
+    printf("对含有 %ld 个整数的两个链表排序后合并花费 %lf ms\n", n * 2,
+           test<LinkedList>(sortMerge<int>, n));
+    srand(seed);
+    printf("对含有 %ld 个整数的两个顺序表合并后排序花费 %lf ms\n", n * 2,
+           test<SequenceList>(mergeSort<int>, n));
+    srand(seed);
+    printf("对含有 %ld 个整数的两个顺序表排序后合并花费 %lf ms\n", n * 2,
+           test<SequenceList>(sortMerge<int>, n));
 }
 
-int main() { finishMyHomework(); }
+int main() {
+    finishMyHomework(100);
+    finishMyHomework(1000);
+    finishMyHomework(10000);
+}
